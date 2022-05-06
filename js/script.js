4eb2cc08fec556d1959dae19f28e9f66be1e8490
kenizaya <harveylstark@gmail.com>
2022-05-06 22:09:38 +0530

    Add playRound()

--- js/script.js
+++ js/script.js
@@ -15,3 +15,27 @@ function computerPlay() {
 
 }
 
+function playRound(playerSelection, computerSelection) {
+    lower = playerSelection.toLowerCase();
+    playerSelection = lower.charAt(0).toUpperCase() + lower.slice(1);
+
+    console.log(playerSelection);
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie";
+    }
+
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock";
+    }   else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You Lose! Scissors beats Paper";
+    }   else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You Lose! Rock beats Scissors";
+    }   else {
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+}
+
+
+
+console.log(playRound("rock", computerPlay()));
